ab4536fc633bd90fe6b1597b6d107f185cef73a6
davimqz <dmo3@cesar.school>
2024-10-14 14:42:26 -0300

    [Refact] - Reinicialização do tudo

--- lavouras/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('home/', views.HomeView, name='home'),
-    path('info_lavouras/', views.LavourasView, name='info_lavouras')
-]
-
--- lavouras/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def HomeView(request):
-    return render (request, 'lavouras_home.html')
-
-def LavourasView(request):
-    return render (request, 'info_lavouras.html')
