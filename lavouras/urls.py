d09395166bad7baf3e2137c4ebf5835b67b3bf6a
davimqz <dmo3@cesar.school>
2024-10-14 08:49:01 -0300

    [Feat] - Adição do Html e do CSS das informações das Lavouras

--- lavouras/urls.py
+++ lavouras/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.HomeView, name='home'),
-
+    path('info_lavouras/', views.LavourasView, name='info_lavouras')
 ]
 
--- lavouras/views.py
+++ lavouras/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def HomeView(request):
     return render (request, 'lavouras_home.html')
+
+def LavourasView(request):
+    return render (request, 'info_lavouras.html')
