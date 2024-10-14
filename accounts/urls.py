78f6ef4161ba7678ac960bedbfe93d4964154f8b
davimqz <dmo3@cesar.school>
2024-10-14 07:43:30 -0300

    [Feat] - Adiçaõ do HTML e CSS da Home

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView)
+    path('', views.HomeView, name='home'),
+    path('registro/', views.RegistroView, name='registro'),
+    path('login/', views.LoginView, name='login'),
 ]
+
