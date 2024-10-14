9c1095813bcc5c4e66cbba1aeefd05dffd5e81cc
davimqz <dmo3@cesar.school>
2024-10-14 16:22:35 -0300

    [Feat] -  Implementação HTML e CSS do registro

--- contas/urls.py
+++ contas/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.RegistroView, name='registro'),
+    path('registro/', views.RegistroView, name='registro'),
     path('login/', views.LoginView, name='login'),
     path('cadastro/', views.CadastroView, name='cadastro')
 ]
