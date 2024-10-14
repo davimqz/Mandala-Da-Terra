2a43c07156b056635ba6b0fb527f5cb34dd0808b
Gabriel Marques <89196175+gabrielmdev@users.noreply.github.com>
2024-10-14 20:34:02 -0300

    att sistema login e registro

--- contas/urls.py
+++ contas/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    # path('registro/', views.RegistroView, name='registro'),
     path('login/', views.login_view, name='login'),  
     path('cadastro/', views.cadastro_view, name='cadastro'),
     path('logout/', views.logout, name='logout'),
