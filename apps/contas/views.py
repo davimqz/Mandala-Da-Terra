3cd663b3e4d3e8e6ed40ba513605eeeb3a46e3a3
davimqz <dmo3@cesar.school>
2024-10-16 09:29:43 -0300

    [Feat] - Adição das views do Login e Cadastro

--- apps/contas/views.py
+++ apps/contas/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login
+from django.http import HttpResponse
+from django.contrib import messages
 
 def RegistroView(request):
     return render (request, 'registro.html')
@@ -9,3 +13,40 @@ def LoginView(request):
 def CadastroView(request):
     return render (request, 'cadastro.html')
 
+def Login(request):
+    if request.method == 'GET':
+        return render (request, 'login.html')
+    else:
+        cpf = request.POST.get('cpf')
+        senha = request.POST.get('senha')
+
+        user = authenticate(request, cpf=cpf, senha=senha)
+
+        if user is not None:
+            auth_login(request, user)
+        else:
+            return HttpResponse('CPF ou senha inválidos')
+        
+def Cadastro(request):
+    if request.method == 'GET':
+        return render (request, 'cadastro.html')
+    else:
+        nome = nome.POST.get('nome')
+        data_nascimento = data_nascimento.POST.get('data_nascimento')
+        cpf = cpf.POST.get('cpf')
+        senha = senha.POST.get('senha')
+
+        user = User.objects.filter(cpf=cpf).first()
+
+        if user:
+            return HttpResponse ('Já existe um usuário com esse CPF')
+        
+        user = User.objects.create_user(
+            nome=nome,
+            data_nascimento=data_nascimento,
+            cpf=cpf,
+            senha=senha,
+        )
+        user.save()
+
+        return render (request, 'login.html', {'sucess_message':'Usuário cadastrado com sucesso!'})
