17a8e4855d7c25d64d88ef8d110f4c5d4f34570e
davimqz <dmo3@cesar.school>
2024-10-16 09:06:41 -0300

    [Feat] - Adição dos Models de Plantacoes e da view de Adicionar Plantação

--- apps/plantacoes/models.py
+++ apps/plantacoes/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class Plantacao(models.Model):
+    SAF = 'Saf'
+    HORTA = 'Horta'
+    RUA1 = 'Rua1'
+    RUA2 = 'Rua2'
+    RUA3 = 'Rua3'
+    RUA4 = 'Rua4'
+    RUA5 = 'Rua5'
+
+    STATUS_CHOICES_TIPO = [
+        (SAF, 'Saf'),
+        (HORTA, 'Horta'),
+    ]
+
+    STATUS_CHOICES_RUAS = [
+        (RUA1, 'Rua1'),
+        (RUA2, 'Rua2'),
+        (RUA3, 'Rua3'),
+        (RUA4, 'Rua4'),
+        (RUA5, 'Rua5'),
+    ]
+
+    nome = models.CharField(max_length=25, null=True, blank=True)
+    tipo = models.CharField(
+        max_length=12,
+        choices=STATUS_CHOICES_TIPO,
+        default=None,
+    )
+    rua = models.CharField(
+        max_length=12,
+        choices=STATUS_CHOICES_RUAS,
+        default=None,
+    )
+    data_colheita = models.DateField
+    data_regada = models.DateField
+
+    def __str__(self):
+        return self.nome
+
+   
--- apps/plantacoes/views.py
+++ apps/plantacoes/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.decorators import login_required
+from . models import Plantacao
 
 def HomeView(request):
     return render (request, 'home.html')
@@ -11,3 +14,26 @@ def HortaView(request):
 
 def AdicionarPlantacaoView(request):
     return render (request, 'adicionar_plantacao.html')
+
+@login_required(login_url='login')
+def AdicionarPlantacao(request):
+    if request.method == 'POST':
+        nome_plantacao = request.POST.get('nome')
+        tipo_plantacao = request.POST.get('tip')
+        rua = request.POST.get('rua')
+        data_colheita = request.POST.get('data_colheita')
+        data_regada = request.POST.get('data_regada')
+
+        plantacao = Plantacao(
+            nome_plantacao=nome_plantacao,
+            tipo_plantacao=tipo_plantacao,
+            rua=rua,
+            data_colheita=data_colheita,
+            data_regada=data_regada,
+            
+        )
+        plantacao.save()
+
+        return redirect('plantacoes/home')
+    
+    return render(request, 'plantacoes/adicionar_plantacao.html')
