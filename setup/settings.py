da970ec61cb5fd0e6f2aa4fc2d1bae2f5a50352b
Davi Marques Oliveira <160409868+davimqz@users.noreply.github.com>
2024-10-18 00:53:00 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -38,11 +38,15 @@ else:
         SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('DBNAME'),
+            'HOST': os.environ.get('DBHOST'),
+            'USER': os.environ.get('DBUSER'),
+            'PASSWORD': os.environ.get('DBPASS'),
+            'OPTIONS': {'sslmode': 'require'},
+        }
     }
-}
 
     
 # Application definition
