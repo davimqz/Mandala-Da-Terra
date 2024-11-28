69d8a988163f9837d8dffbbfaf27f9cd12c80b26
Davi Marques Oliveira <160409868+davimqz@users.noreply.github.com>
2024-11-28 16:23:55 -0300

    Add files via upload
    Mudança dos arquivos da branch gabriel para a prod

--- project/setup/settings.py
+++ project/setup/settings.py
@@ -141,7 +141,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'apps/contas/static',
+    os.path.join(BASE_DIR, "static"),  # Caminho para os arquivos estáticos da aplicação
 ]
 
 
